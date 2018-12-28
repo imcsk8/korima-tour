15e59189c2f9af9aad5ecaef759ebfceab604d0d
Ivan Chavero <ichavero@redhat.com>
2018-12-28 14:08:29 -0700

    Add show action for bands
    - Add show action for bands
    - Fix bands actions url

--- korima_hishow/actions/bands.go
+++ korima_hishow/actions/bands.go
@@ -4,25 +4,30 @@ import "github.com/gobuffalo/buffalo"
 
 // BandsIndex default implementation.
 func BandsIndex(c buffalo.Context) error {
-	return c.Render(200, r.HTML("bandas/index.html"))
+	return c.Render(200, r.HTML("bands/index.html"))
+}
+
+// BandsCreateShow shows the band creation form
+func BandsCreateShow(c buffalo.Context) error {
+	return c.Render(200, r.HTML("bands/create.html"))
 }
 
 // BandsCreate default implementation.
 func BandsCreate(c buffalo.Context) error {
-	return c.Render(200, r.HTML("bandas/create.html"))
+	return c.Render(200, r.HTML("bands/create.html"))
 }
 
 // BandsEdit default implementation.
 func BandsEdit(c buffalo.Context) error {
-	return c.Render(200, r.HTML("bandas/edit.html"))
+	return c.Render(200, r.HTML("bands/edit.html"))
 }
 
 // BandsDelete default implementation.
 func BandsDelete(c buffalo.Context) error {
-	return c.Render(200, r.HTML("bandas/delete.html"))
+	return c.Render(200, r.HTML("bands/delete.html"))
 }
 
 // BandsDetail default implementation.
 func BandsDetail(c buffalo.Context) error {
-	return c.Render(200, r.HTML("bandas/detail.html"))
+	return c.Render(200, r.HTML("bands/detail.html"))
 }
