8c38af47a286b6f96e976a7443054a035b1df030
Ivan Chavero <ichavero@redhat.com>
2018-12-30 16:07:45 -0700

    Add generic functions for photo update

--- korima_hishow/models/models.go
+++ korima_hishow/models/models.go
@@ -1,11 +1,25 @@
 package models
 
 import (
+	"io"
 	"log"
+	"os"
+	"path/filepath"
 
+	"github.com/gobuffalo/buffalo/binding"
 	"github.com/gobuffalo/envy"
 	"github.com/gobuffalo/pop"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
+
+// Field allows us to have generic functions for database fields
+type field interface {
+	SetPhotoName() error
+	ValidPhoto() bool
+	GetPhotoFileName() string
+	GetPhotoStream() binding.File
+}
 
 // DB is a connection to your database to be used
 // throughout your application.
@@ -19,4 +33,29 @@ func init() {
 		log.Fatal(err)
 	}
 	pop.Debug = env == "development"
+}
+
+// BeforeSave is a callback that preprocess the struct before saving it to the database
+func BeforeSaveFile(f field) error {
+	f.SetPhotoName()
+	return nil
+}
+
+// AfterSave is a callback that saves the file after the venue is saved to the database
+func AfterSaveFile(f field) error {
+	if !f.ValidPhoto() {
+		logrus.Infof("Invalid photo file: %v", f)
+		return nil
+	}
+	dir := filepath.Join(".", "public/uploads/photos")
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return errors.WithStack(err)
+	}
+	pf, err := os.Create(filepath.Join(dir, f.GetPhotoFileName()))
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer pf.Close()
+	_, err = io.Copy(pf, f.GetPhotoStream())
+	return err
 }
