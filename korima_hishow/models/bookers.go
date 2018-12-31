8f6904d25cfaaff55f5cb5021030a026aff393c2
Ivan Chavero <ichavero@redhat.com>
2018-12-30 17:06:02 -0700

    Add photo to booker model

--- korima_hishow/models/bookers.go
+++ korima_hishow/models/bookers.go
@@ -3,6 +3,7 @@ package models
 import (
 	"time"
 
+	"github.com/gobuffalo/buffalo/binding"
 	"github.com/gobuffalo/pop"
 	uuid "github.com/gobuffalo/uuid"
 	"github.com/gobuffalo/validate"
@@ -10,12 +11,14 @@ import (
 )
 
 type Booker struct {
-	ID          uuid.UUID `json:"id" db:"id"`
-	CreatedAt   time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at" db:"updated_at"`
-	Name        string    `json:"title" db:"name"`
-	Description string    `json:"content" db:"description"`
-	OwnerID     uuid.UUID `json:"owner_id" db:"owner_id"`
+	ID          uuid.UUID    `json:"id" db:"id"`
+	CreatedAt   time.Time    `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at" db:"updated_at"`
+	Name        string       `json:"title" db:"name"`
+	Description string       `json:"content" db:"description"`
+	Photo       string       `json:"photo" db:"photo"`
+	PhotoFile   binding.File `json:"photo_file" db:"-" form:"photo_file"`
+	OwnerID     uuid.UUID    `json:"owner_id" db:"owner_id"`
 }
 
 type Bookers []Booker
@@ -26,4 +29,35 @@ func (b *Booker) Validate(tx *pop.Connection) (*validate.Errors, error) {
 		&validators.StringIsPresent{Field: b.Name, Name: "Name"},
 		&validators.StringIsPresent{Field: b.Description, Name: "Description"},
 	), nil
+}
+
+// BeforeSave is a callback that preprocess the struct before saving it to the database
+func (b *Booker) BeforeSave(tx *pop.Connection) error {
+	return BeforeSaveFile(b)
+}
+
+// AfterSave is a callback that saves the file after the venue is saved to the database
+func (b *Booker) AfterSave(tx *pop.Connection) error {
+	return AfterSaveFile(b)
+}
+
+// SetPhotoName sets the photo filename parameter in the Photo field of te database
+func (b *Booker) SetPhotoName() error {
+	b.Photo = b.PhotoFile.Filename
+	return nil
+}
+
+// ValidPhoto returns true if there's an uploaded photo from the form
+func (b *Booker) ValidPhoto() bool {
+	return b.PhotoFile.Valid() && b.PhotoFile.Filename != ""
+}
+
+// GetPhotoFilename returns the filename of the uploaded photo
+func (b *Booker) GetPhotoFileName() string {
+	return b.PhotoFile.Filename
+}
+
+// GetPhotoStream returns the binding.File field
+func (b *Booker) GetPhotoStream() binding.File {
+	return b.PhotoFile
 }
